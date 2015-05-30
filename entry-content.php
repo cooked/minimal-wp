29b430a1fec7e5b6674eab9ccc6987b312a6ff57
stefano.cottafavi <stefano.cottafavi@gmail.com>
2015-05-30 20:33:28 +0100

    modified read more tag to "continue...", added <pre> tag styling

--- entry-content.php
+++ entry-content.php
@@ -5,5 +5,5 @@
 	</div>
 <?php endif; ?>
 <div class="entry-links"><?php wp_link_pages('before=<p>&after=&next_or_number=next&nextpagelink=&previouspagelink=<'); wp_link_pages('before=&after='); wp_link_pages('before=&after=</p>&next_or_number=next&previouspagelink=&nextpagelink=>'); ?></div>
-<?php the_content(); ?>
+<?php the_content('continue...'); ?>
 </section>
