ddf8d017a19f2df76012304e7a3f13f786c488e1
stefano.cottafavi <stefano.cottafavi@gmail.com>
2015-05-30 13:51:54 +0100

    removed icon dependancy

--- searchform.php
+++ searchform.php
@@ -1,6 +1,10 @@
 <form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
      <fieldset>
          <input type="search" id="s" name="s" placeholder="" required />
-         <input type="image" id="searchsubmit" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/icons/noun_search.png" />
+         <input type="submit" id="searchsubmit" value="go">
+         <!-- 
+         
+         <input type="submit" id="searchsubmit" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/icons/noun_search.png" /> -->
+     
      </fieldset>
 </form>
