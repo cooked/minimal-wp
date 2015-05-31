506d12dc43db09b34a44afb0ee270fd323babb62
stefano.cottafavi <stefano.cottafavi@gmail.com>
2015-05-31 23:41:11 +0100

    again sidebars

--- functions.php
+++ functions.php
@@ -1,19 +1,5 @@
 <?php
 
-// see https://themes.trac.wordpress.org/ticket/20672
-add_action( 'widgets_init', 'minimal_wp_widget' );
-function minimal_wp_widget(){
-	register_sidebar( array(
-			'name'          => __( 'Primary Sidebar', 'minimal_wp' ),
-			'id'            => 'primary-widget-area',
-			'description'   => __( 'sidebar', 'minimal_wp' ),
-			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</aside>',
-			'before_title'  => '<h1 class="widget-title">',
-			'after_title'   => '</h1>',
-	) );
-}
-
 add_action ( 'after_setup_theme', 'minimal_wp_setup' );
 function minimal_wp_setup() {
 	load_theme_textdomain( 'minimal_wp', get_template_directory () . '/languages' );
@@ -30,6 +16,19 @@ function minimal_wp_setup() {
 			'main-menu' => __ ( 'Main Menu', 'minimal_wp' ) 
 	) );
 }
+
+function minimal_wp_widgets_init() {
+	register_sidebar( array(
+			'name' 			=> __( 'Sidebar Widget Area', 'minimal_wp' ),
+			'id' 			=> 'primary-widget-area',
+			'description'   => __( 'sidebar', 'minimal_wp' ),
+			'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+			'after_widget' 	=> "</li>",
+			'before_title' 	=> '<h3 class="widget-title">',
+			'after_title' 	=> '</h3>'
+	) );
+}
+add_action( 'widgets_init', 'minimal_wp_widgets_init' );
 
 add_action ( 'wp_enqueue_scripts', 'minimal_wp_load_scripts' );
 function minimal_wp_load_scripts() {
@@ -71,6 +70,29 @@ function minimal_wp_comments_number($count) {
 	}
 }
 
+
+/**
+ * Dont Update the Theme
+ *
+ * If there is a theme in the repo with the same name, this prevents WP from prompting an update.
+ *
+ * @since  1.0.0
+ * @param  array $r Existing request arguments
+ * @param  string $url Request URL
+ * @return array Amended request arguments
+
+function ea_dont_update_theme( $r, $url ) {
+	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
+ 		return $r; // Not a theme update request. Bail immediately.
+ 	$themes = json_decode( $r['body']['themes'] );
+ 	$child = get_option( 'stylesheet' );
+	unset( $themes->themes->$child );
+ 	$r['body']['themes'] = json_encode( $themes );
+ 	return $r;
+ }
+ add_filter( 'http_request_args', 'ea_dont_update_theme', 5, 2 );
+*/
+ 
  /**
   * Snippet Name: Add page break button to tinyMCE editor bar
   * Snippet URL: http://www.wpcustoms.net/snippets/add-page-break-button-to-tinymce-editor-bar/
