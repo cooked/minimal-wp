93412d9b86e1c14a4e452a6a6f7b11c2dd3f52c3
stefano.cottafavi <stefano.cottafavi@gmail.com>
2015-05-31 17:37:37 +0100

    <section> to <div> in entry-meta

--- entry-meta.php
+++ entry-meta.php
@@ -1,4 +1,4 @@
-<section class="entry-meta">
+<div class="entry-meta">
 <span class="meta-sep"></span>
 <span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
-</section>
+</div>
--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
      <fieldset>
          <input type="search" id="s" name="s" placeholder="" required />
-         <input type="submit" id="searchsubmit" value="go">
+         <input type="submit" id="searchsubmit" value="go" />
          <!-- 
          
          <input type="submit" id="searchsubmit" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/icons/noun_search.png" /> -->
