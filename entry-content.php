10f26a27acde209dac28dc3fd07c2bb97987cde8
stefano.cottafavi <stefano.cottafavi@gmail.com>
2015-05-31 17:39:51 +0100

    <section> to <div> to validate

--- entry-content.php
+++ entry-content.php
@@ -1,4 +1,4 @@
-<section class="entry-content">
+<div class="entry-content">
 <?php if (has_post_thumbnail()): ?>
 	<div class="thumb">
 	<?php the_post_thumbnail(); ?>
@@ -6,4 +6,4 @@
 <?php endif; ?>
 <div class="entry-links"><?php wp_link_pages('before=<p>&after=&next_or_number=next&nextpagelink=&previouspagelink=<'); wp_link_pages('before=&after='); wp_link_pages('before=&after=</p>&next_or_number=next&previouspagelink=&nextpagelink=>'); ?></div>
 <?php the_content('continue...'); ?>
-</section>
+</div>
