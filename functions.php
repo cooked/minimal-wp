11a1a8af0ecd23c70b3e170594a8800a939268a1
stefano.cottafavi <stefano.cottafavi@gmail.com>
2015-06-01 01:28:36 +0100

    latest sidebar fix

--- functions.php
+++ functions.php
@@ -1,10 +1,12 @@
 <?php
-
 add_action ( 'after_setup_theme', 'minimal_wp_setup' );
 function minimal_wp_setup() {
 	load_theme_textdomain( 'minimal_wp', get_template_directory () . '/languages' );
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( "title-tag" );
+	add_theme_support( "custom-header" );
+	add_theme_support( "custom-background" );
 	add_editor_style('style.css'); // add own stylesheet to WordPress Editor
 	global $content_width;
 	if (! isset ( $content_width ))
@@ -13,8 +15,6 @@ function minimal_wp_setup() {
 			'main-menu' => __ ( 'Main Menu', 'minimal_wp' ) 
 	) );
 }
-
-
 
 add_action ( 'wp_enqueue_scripts', 'minimal_wp_load_scripts' );
 function minimal_wp_load_scripts() {
@@ -27,7 +27,6 @@ function minimal_wp_enqueue_comment_reply_script() {
 		wp_enqueue_script ( 'comment-reply' );
 	}
 }
-
 
 add_filter ( 'the_title', 'minimal_wp_title' );
 function minimal_wp_title($title) {
@@ -43,8 +42,26 @@ function minimal_wp_filter_wp_title($title) {
 	return $title . esc_attr ( get_bloginfo ( 'name' ) );
 }
 
-
-
+// see https://themes.trac.wordpress.org/ticket/20672
+// NOTE: seriously??? with a space between add_action and ( will fail WP validation !?!?!?!?!
+add_action( 'widgets_init', 'minimal_wp_widget' );
+function minimal_wp_widget() {
+	register_sidebar( array(
+			'name' 			=> __( 'Sidebar Widget Area', 'minimal_wp' ),
+			'id' 			=> 'primary-widget-area',
+			'description'   => __( 'sidebar', 'minimal_wp' ),
+			'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+			'after_widget' 	=> "</li>",
+			'before_title' 	=> '<h3 class="widget-title">',
+			'after_title' 	=> '</h3>' 
+	) );
+}
+function minimal_wp_custom_pings($comment) {
+	$GLOBALS ['comment'] = $comment;
+	?>
+<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
+<?php
+}
 add_filter ( 'get_comments_number', 'minimal_wp_comments_number' );
 function minimal_wp_comments_number($count) {
 	if (! is_admin ()) {
@@ -55,29 +72,6 @@ function minimal_wp_comments_number($count) {
 		return $count;
 	}
 }
-
-
-/**
- * Dont Update the Theme
- *
- * If there is a theme in the repo with the same name, this prevents WP from prompting an update.
- *
- * @since  1.0.0
- * @param  array $r Existing request arguments
- * @param  string $url Request URL
- * @return array Amended request arguments
-
-function ea_dont_update_theme( $r, $url ) {
-	if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) )
- 		return $r; // Not a theme update request. Bail immediately.
- 	$themes = json_decode( $r['body']['themes'] );
- 	$child = get_option( 'stylesheet' );
-	unset( $themes->themes->$child );
- 	$r['body']['themes'] = json_encode( $themes );
- 	return $r;
- }
- add_filter( 'http_request_args', 'ea_dont_update_theme', 5, 2 );
-*/
  
  /**
   * Snippet Name: Add page break button to tinyMCE editor bar
@@ -93,4 +87,5 @@ function wpc_nextpage_tinyMCE($mce_buttons) {
  	}
 	return $mce_buttons;
 }
+ 
  
