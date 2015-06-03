f1f1c60dd03ce3bab2e7cf585b385d606436124b
stefano.cottafavi <stefano.cottafavi@gmail.com>
2015-06-03 23:07:56 +0100

    footer admin theme page, embedded creative commons font

--- footer.php
+++ footer.php
@@ -2,7 +2,9 @@
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'minimal_wp' ), '&copy;', date( 'Y' ),esc_html( get_bloginfo( 'name' ) )  ); ?>
+<?php 
+	$options = get_option( 'pu_theme_options' );
+	echo sprintf( __( '%1$s %2$s %3$s.', 'minimal_wp' ), $options['pu_textbox'] , date( 'Y' ),esc_html( get_bloginfo( 'name' ) )  ); ?>
 </div>
 </footer>
 </div>
