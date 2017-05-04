5fd7bb57b6f364c573195115d60dffa04f4d7999
stefano.cottafavi <stefano.cottafavi@gmail.com>
2017-05-05 01:27:48 +0200

    menu moved to PHP code widget, theme is even more minimal

--- header.php
+++ header.php
@@ -19,49 +19,7 @@
 				<div id="site-description"><?php bloginfo( 'description' ); ?></div>
 			</section>
 			
-			<nav id="menu" role="navigation">
-				<nav class="primary_nav_wrap">
-					<ul>
-						<?php
-							$minimal_wp_variable = wp_list_categories('echo=0&orderby=name');
-							$minimal_wp_variable = preg_replace('~\((\d+)\)(?=\s*+<)~', '<div class="num">$1</num>', $minimal_wp_variable);
-							echo $minimal_wp_variable;
-						?>
-					</ul>
-				</nav>
-				<nav class="primary_nav_wrap">
-					<ul>
-						<li>Tags
-							<?php wp_tag_cloud('format=list&orderby=count&order=DESC&smallest=10&largest=10&number=15'); ?>
-						 </li>
-					</ul>
-				</nav>
-				<nav class="primary_nav_wrap">
-					<ul>
-						<li>Archive
-							<ul class="archives">
-								<?php wp_get_archives('type=monthly&limit=12'); ?>
-							</ul>
-						</li>
-					</ul>
-				</nav>
-
-				<nav class="primary_nav_wrap nav_search">
-					<ul>
-						<li>Search
-							<ul class="">
-								<li><?php get_search_form(); ?></li>
-							</ul>
-						</li>
-					</ul>
-				</nav>
-				<?php 
-				if(get_page_by_title('About')) : ?>
-					<nav class="primary_nav_wrap"><ul class="nav_about"><li><a href="<?php site_url() ?>/about/">?</a></li></ul></nav>
-				<?php endif;?>
-			</nav>
-			
-			<div id="sidebar-11">
+			<div id="sidebar-1">
 				<?php get_sidebar('sidebar-1'); ?>
 			</div>
 			
